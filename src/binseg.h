2fa398b731e6e2c0f25887435d20211f0fdbe7d5
harsheka <kh2638@nau.edu>
2021-11-05 16:09:46 -0700

    binseg running

---
+++ src/binseg.h
@@ -0,0 +1,9 @@
+
+#define ERROR_TOO_MANY_SEGMENTS 2
+
+int rcpp_binseg
+  (const double *data_vec, const int n_data, const int max_segments,
+   int *seg_end, double *cost,
+   double *before_mean, double *after_mean,
+   int *, int *,
+   int *invalidates_index, int *invalidates_before);
