7399681e618e19c10532e6f7a996a99af57d2861
harsheka <kh2638@nau.edu>
2021-11-05 19:31:13 -0700

    cumsum function

---
+++ src/cum_sum.cpp
@@ -0,0 +1,20 @@
+#include <Rcpp.h>
+#include <vector>
+#include <numeric> 
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+NumericVector cpp_cumsum(NumericVector x){
+  // initialize an accumulator variable
+  double acc = 0;
+  
+  // initialize the result vector
+  NumericVector res(x.size());
+  
+  for(int i = 0; i < x.size(); i++){
+    acc += x[i];
+    res[i] = acc;
+  }
+  return res;
+}
+
