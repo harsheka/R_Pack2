71b5f2162a28dec34f233be65df3b8e6a5cad053
harsheka <kh2638@nau.edu>
2021-11-06 18:06:12 -0700

    DONT RUN THIS ! !

--- src/DynProg.cpp
+++ src/DynProg.cpp
@@ -1,6 +1,29 @@
 #include <Rcpp.h>
 #include "DynProg.h"
+
 using namespace Rcpp;
+
+NumericVector cpp_cum_sum1(NumericVector x){
+   // initialize an accumulator variable
+   double acc = 0;
+   
+   // initialize the result vector
+   NumericVector res(x.size());
+   
+   for(int i = 0; i < x.size(); i++){
+      acc += x[i];
+      res[i] = acc;
+   }
+   return res;
+}  
+
+NumericVector Cost(NumericVector cumsum_vec, NumericVector position_vec){
+  NumericVector Cost_vec(cumsum_vec.length());
+  for(int i; i<cumsum_vec.length(); i++){
+    Cost_vec[i]= (-1*pow(cumsum_vec[i],2))/position_vec[i];
+  }
+  return Cost_vec;
+}
 
 // [[Rcpp::export]]
 Rcpp::NumericVector cpp_dynamic_prog
@@ -8,10 +31,59 @@ Rcpp::NumericVector cpp_dynamic_prog
    const int k_max){
   int n_data = input_vec.length();
  
-  Rcout << n_data;
-  //if (n_data < 1){
-   // return 2;
-  //}
+  //Rcout << n_data <<"\n";
+  
+  //initialize the cost matrix with zeros
+  NumericMatrix cost_mat( n_data, k_max);
+  //
+  //Rcout << cost_mat<<"\n";
+  NumericVector cumsum(cpp_cum_sum1(input_vec));
+   // Rcout << cumsum<<"\n";
+    
+  NumericVector loc(n_data);
+  std::iota(loc.begin(), loc.end(),1);
+  //Rcout<<loc<<"\n";
+  
+  NumericVector our_cost = Cost(cumsum, loc);
+ // Rcout<<our_cost<<"\n";
+  //Rcout << "updated cost mat is:"<<"\n";
+  cost_mat(_,0)= our_cost;
+  //Rcout << cost_mat<<"\n";
+  
+  for(int i=1; i<k_max; i++){
+  
+    int prev_i = i-1;
+  
+    for (int ii = i; ii<n_data; ii++){
+      NumericVector pos_last_end(ii);
+     
+      std::iota(pos_last_end.begin(), pos_last_end.end(),prev_i);
+      
+      NumericVector prev_i_vec(pos_last_end.length(),prev_i);
+      NumericVector all_prev_costs= Cost(pos_last_end, prev_i_vec);
+      
+      NumericVector rev_pos_last_end(ii);
+      std::iota(pos_last_end.begin(), pos_last_end.end(),ii);  // might  be prev_i instead of ii
+      std::reverse(pos_last_end.begin(), pos_last_end.end());
+      NumericVector last_seg_sums(ii);
+      for (int iii = 0; iii< ii; iii++){
+        last_seg_sums[iii] = cumsum[ii] - cumsum[iii];
+      }
+      NumericVector last_costs = Cost(last_seg_sums, rev_pos_last_end);
+      
+      NumericVector tot_cost(ii);
+      for (int iii = 0; iii< ii; iii++){
+        tot_cost[iii] = all_prev_costs[iii] + last_costs[iii];
+      }
+
+      int best_option = min(tot_cost);  // index of minimum val
+      
+      cost_mat(ii,i)= tot_cost[best_option] ;
+    }
+  }
+  
+  //Rcout<< cost_mat<<"\n";
+  
   return 0;
 }
-   
+ 
